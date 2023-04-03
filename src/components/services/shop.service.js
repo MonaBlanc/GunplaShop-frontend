ea74ed25b908f6cc034098b68766e62bdd3122ab
Ayoub <ayoub2001@hotmail.it>
2023-04-03 10:31:15 +0200

    test

--- src/components/services/shop.service.js
+++ src/components/services/shop.service.js
@@ -6,7 +6,6 @@ export const getProducts = (newUser) => {
     // GET request on http://localhost:8080/api/products
     return axios.get(`${baseURL}/products`)
         .then(response => {
-            console.log(response);
             if (response) {
                 return Promise.resolve(response);
             }
--- src/container/actions.js
+++ src/container/actions.js
@@ -8,7 +8,6 @@ export const getProducts = () => (dispatch) => {
     // use redux-thunk (in store.js) to do that
     return ShopService.getProducts()
         .then(response => {
-            console.log(response);
             dispatch({
                 type: actionType.GET_PRODUCTS_SUCCESS,
                 payload: response.data
